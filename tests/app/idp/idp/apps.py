47a35bd4c152b1e112fbc997ea2d62376a433d7e
Darrel O'Pry <darrel.opry@spry-group.com>
2025-11-08 09:12:14 -0500

    chore: add a device code demo to the tests/app/RP

--- tests/app/idp/idp/apps.py
+++ tests/app/idp/idp/apps.py
@@ -3,11 +3,20 @@ from django.apps import AppConfig
 
 
 def cors_allow_origin(sender, request, **kwargs):
+    origin = request.headers.get('Origin')
+
     return (
         request.path == "/o/userinfo/"
         or request.path == "/o/userinfo"
         or request.path == "/o/.well-known/openid-configuration"
         or request.path == "/o/.well-known/openid-configuration/"
+        # this is for testing the device authorization flow in the example rp.
+        # You would not normally have a browser-based client do this and shoudn't
+        # open the following endpoints to CORS requests in a production environment.
+        or (origin == 'http://localhost:5173' and request.path == "/o/device-authorization")
+        or (origin == 'http://localhost:5173' and request.path == "/o/device-authorization/")
+        or (origin == 'http://localhost:5173' and request.path == "/o/token")
+        or (origin == 'http://localhost:5173' and request.path == "/o/token/")
     )
 
 
